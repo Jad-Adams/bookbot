edd88a62a3b1fab97ad6342b847c9873424e6dfb
Jad-Adams <iam.jadadams@gmail.com>
2024-09-29 19:35:34 +0200

    can print word count

---
+++ main.py
@@ -0,0 +1,10 @@
+def main():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+        
+    def word_count(book):
+        words = book.split()
+        print(len(words))
+        
+    word_count(file_contents)        
+main()

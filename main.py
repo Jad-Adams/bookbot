b85e3238091db79e66bc600b8e6d7a5e9e689e68
Jad-Adams <iam.jadadams@gmail.com>
2024-10-01 20:22:55 +0200

    can play back number of characters

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
 def main():
     book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
-    num_words = word_count(text)
+    book_text = get_book_text(book_path)
+    num_words = word_count(book_text)
     print(f"this book contains {num_words} words")
-    
+    lower_case_text = to_lowercase(book_text)
+    char_count(lower_case_text)
+
+# Get the book    
 def get_book_text(path):
     with open(path) as f:
         file_contents = f.read()
         return file_contents
 
+# Calculates number of words in text
 def word_count(book):
     words = book.split()
     return(len(words))
+
+# Converts text to all lower case
+def to_lowercase(text):
+    lowered_string = text.lower()
+    return lowered_string
+
+# Calcualtes num of times a letter occurs in a text
+def char_count(x):
+    dict_characters = {}
+    
+    for character in x:
+        if character in dict_characters:
+            dict_characters[character] += 1
+        else:
+            dict_characters[character] = 1
+    
+    print(dict_characters)
 
 main()
